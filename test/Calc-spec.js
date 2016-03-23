3397ccb958982ca3882f731650d24648e0bc8926
Niv Tschaikov <niv@sealights.io>
2016-03-23 11:08:45 +0200

    all

---
+++ test/Calc-spec.js
@@ -0,0 +1,16 @@
+var Calc = require('../lib/Calc');
+
+console.log("hi");
+
+describe("Calc-spec", function () {
+    it("plus", function (done) {
+    	var calc = new Calc();
+    	var c = calc.plus(2, 3);
+    	done()
+    });
+    it("minus", function (done) {
+    	var calc = new Calc();
+    	var c = calc.minus(5, 3);
+    	done();
+    });
+});
